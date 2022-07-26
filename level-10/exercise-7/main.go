284456bd7df81873e9617fec1c2071c9428ee36e
Anton Pavlov <dharlequin@gmail.com>
2022-07-26 12:27:57 +0300

    it's not an elegant solution, but I made the exercise 7 work

--- level-10/exercise-7/main.go
+++ level-10/exercise-7/main.go
@@ -2,27 +2,20 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 func main() {
-	var wg sync.WaitGroup
-
 	c := make(chan int, 10)
 
 	for i := 0; i < 10; i++ {
-		wg.Add(1)
 
 		go func() {
-			defer wg.Done()
 			addNumber(c)
 		}()
 	}
 
-	wg.Wait()
-
-	for v := range c {
-		fmt.Println(v)
+	for v := 0; v < 100; v++ {
+		fmt.Println(<-c)
 	}
 }
 
